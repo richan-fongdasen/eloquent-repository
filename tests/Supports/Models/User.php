c8f20bd1d03f27ba36677aa73de60a56a223bf90
Richan Fongdasen <richan.fongdasen@gmail.com>
2018-06-16 13:05:36 +0700

    Remove Authenticable and Notifiable from test User Model.

--- tests/Supports/Models/User.php
+++ tests/Supports/Models/User.php
@@ -2,14 +2,11 @@
 
 namespace RichanFongdasen\Repository\Tests\Supports\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
